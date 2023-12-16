3cb2ba5c775ead84e73a7c2686161c78902864c7
Kriish sharma <kriish.sharma2006@gmail.com>
2023-12-16 21:15:10 +0530

    Update stringcube.py

--- stringcube.py
+++ stringcube.py
@@ -1,21 +1,8 @@
-import math as m
-l = []
+i = input("enter  ").split()
+for x in i :
+    print(int(x)**3)
 
-while True:
-    f = input("Enter an element (to stop, write 'done'): ")
-    t = input("Continue? (yes or no): ")
-    
-    if t.lower() != 'no':
-        l.append(f)  
-    else:
-        break
+# or 
 
-print("Original list:", l)
-
-l1 = []
-
-for i in l:
-    if (int(i) % 2) == 0:
-        l1.append(int(i)**3)  # Calculate the cube of the even number
-
-print("Cubes of even numbers:", l1)
+e = [int(x)**3 for x in i]
+print(e)
